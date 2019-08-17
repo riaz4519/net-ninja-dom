da0f44c0cd795e22ce1ca1da79afb1ae436a0fdc
unknown <riaz.i3216@gmail.com>
2019-08-17 12:19:36 +0600

    after 5

--- code/app.js
+++ code/app.js
@@ -1,3 +1,4 @@
+/*
 var wmf = document.querySelector('#book-list li:nth-child(2) .name');
 
 //console.log(wmf);
@@ -12,4 +13,18 @@ Array.from(books).forEach(function (book) {
 
 console.log(book);
 
+});*/
+
+
+/*5*/
+var books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach(function (book) {
+
+    book.textContent += '(book title)';
 });
+
+const bookList = document.querySelector('#book-list');
+
+bookList.innerHTML = "<h1>Hello world</h1>"
+
